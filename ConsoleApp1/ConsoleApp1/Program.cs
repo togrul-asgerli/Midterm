1bfd5439cf56dc745c40bdfdea8eb9c365190733
togrul-asgerli <81424132+togrul-asgerli@users.noreply.github.com>
2022-10-31 12:44:33 +0400

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Globalization;
+using System.Data.SqlTypes;
+using System.Collections;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            /* Find vowels number in given string*/
+            List <string> list = new List<string>();
+
+            for(int i=0;i<5;i++)
+            {
+                list.Add(Console.ReadLine());
+            }
+            foreach(var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Convert.ToInt32(Console.ReadLine());
+            Console.ReadKey();
+        }
+
+    }
+}
